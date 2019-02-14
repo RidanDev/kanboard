99c15ccf74b5adfc8ba0eee192a7768d0fe3c0a9
Ojini Chizoba Jude <judoc96@gmail.com>
2019-02-14 14:55:05 +0100

    setup for user controller and model

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,23 +2,57 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../model/user.model");
 const asyncHandler = require("../lib/async_handler");
+const setCookie = require("../lib/cookie_token");
 
-exports.getUsers = () => {};
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  //TODO
+  // check if user is logged in
+  // check if user have the necessary priviledges
+  // get all the users
+  const users = await User.find({}).select("username email name _id");
+  res.json({
+    count: users.length,
+    users
+  });
+});
 
 exports.createUser = asyncHandler(async (req, res, next) => {
+  const usernameExist = await User.findOne({ username: req.body.username });
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) throw new Error("Email already exist");
+  if (usernameExist) throw new Error("Username already exist");
+
   let password = await bcrypt.hash(req.body.password, 10);
-
   const user = await User.create({ ...req.body, password });
+  const set = await setCookie(user, res);
+  if (set) return res.json({ message: "user created successfully" });
+});
 
-  let token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+exports.loginUser = asyncHandler(async (req, res, next) => {
+  if (!req.body.email || !(req.body.email.length > 0)) return res.json({ message: "email field should not be empty" });
 
-  res.cookie("kanbanToken", token, {
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
-  });
+  if (!req.body.password) return res.json({ message: "password field should not be empty" });
 
-  res.json({
-    message: "user created successfully",
-    user
-  });
+  const user = await User.findOne({ email: req.body.email.toLowerCase() });
+  if (!user) return res.json({ message: "email or password is incorrect" });
+
+  const confirmPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!confirmPassword) return res.json({ message: "email or password is incorrect" });
+
+  const set = await setCookie(user, res);
+  if (set) return res.json({ message: "User successfully logged in" });
+});
+
+exports.logoutUser = asyncHandler(async (req, res, next) => {
+  res.clearCookie("kanboardToken");
+  res.json({ message: "User successfully logged out" });
+});
+
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  //TODO
+  // check if the user is logged in
+  // check if the user have the necessary permissions to delete
+  // delete the specified user
+  const deleted = await User.findByIdAndDelete(req.params.id);
+  if (deleted) return res.json({ message: "User successfully deleted" });
 });
--- model/user.model.js
+++ model/user.model.js
@@ -7,6 +7,11 @@ const UserSchema = new mongoose.Schema({
     type: String,
     maxlength: 225
   },
+  name: {
+    required: true,
+    type: String,
+    maxlength: 300
+  },
   email: {
     required: true,
     unique: true,
@@ -18,6 +23,16 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+});
+
+UserSchema.pre("save", function(next) {
+  let namesArray = this.name.split(" ");
+  namesArray = namesArray.map(name => {
+    //capitalize the first letters
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  });
+  this.name = namesArray.join(" ").toString();
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
