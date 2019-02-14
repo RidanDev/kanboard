8fc453458e2d37e51eaf4ef716fb15884b6a0865
Ojini Chizoba Jude <judoc96@gmail.com>
2019-02-14 14:52:52 +0100

    route setup for user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,10 @@ const user = require("../controller/user.controller");
 
 const route = express.Router();
 
+route.get("/users", user.getUsers);
+route.delete("/users/:id", user.deleteUser);
+route.post("/user/logout", user.logoutUser);
 route.post("/user/signup", user.createUser);
+route.post("/user/login", user.loginUser);
 
 module.exports = route;
