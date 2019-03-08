3eb6f74be3a1d819fe86d5e43d991625ccc5a106
Ojini Chizoba Jude <judoc96@gmail.com>
2019-03-08 17:30:51 +0100

    remove server.js file in client

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(function(req, res, next) {
   // This cors setting makes sure that cookie is set on the client's device
   // In the frontend make sure to set withCredentials: true, so that cookie can be sent along on network request
-  // res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "http://localhost:4500");
+  res.header("Access-Control-Allow-Origin", "https://kanboarding.herokuapp.com");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "HEAD, GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
